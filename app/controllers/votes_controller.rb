c85e8c5c4afde8f7ad2e86c6a3ba8c324f864df1
walter-petkanych <silwerfild@gmail.com>
2021-02-17 21:56:25 +0200

    corrected votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,13 @@ class VotesController < ApplicationController
 
   def new
     @vote = Vote.new
+    @micropost_id = params[:micropost_id]
+  end
+
+  def create
+    puts(params)
+    Vote.create({user_id: params[:a], micropost_id: params[:micropost_id], vote: params[:b]})
+    redirect_to '/microposts'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :microposts
   get "votes/new", to: "votes#new"
+  post "votes/create", to: "votes#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
